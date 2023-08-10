345612e7810d465ec6964441f45893f94fa0fba5
hmourafisoft <henrique.moura@fisoft.com.br>
2023-08-10 08:40:06 -0300

    Initial commit

---
+++ jogos.py
@@ -0,0 +1,18 @@
+import forca
+import advinhacao
+
+
+print("************************************")
+print("*********Escolha seu jogo!**********")
+print("************************************")
+
+print("(1)Forca (2)Advinhação")
+
+jogo = int(input("Qual jogo?"))
+
+if(jogo ==1):
+   print("Jogando Forca")
+   forca.jogar()
+elif(jogo == 2):
+   print("Jogando Advinhação")
+   advinhacao.jogar()
